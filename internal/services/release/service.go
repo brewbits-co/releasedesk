4b45bd05fc280d15a2fb5353ec51c34fe7e4425c
isflavior <flavio.reis@artsoft.pt>
2025-05-09 00:54:37 +0100

    add release notes service methods

--- internal/services/release/service.go
+++ internal/services/release/service.go
@@ -12,17 +12,43 @@ type Service interface {
 	ListReleasesByChannel(appID int, channelID int) ([]release.BasicInfo, error)
 	GetReleaseSummary(appID int, version string) (release.Release, error)
 	GetReleaseChannels(appID int) ([]release.Channel, error)
+	SaveReleaseNotes(releaseID int, text string, changelogs []release.Changelog) (release.ReleaseNotes, error)
+	GetReleaseNotes(releaseID int) (release.ReleaseNotes, error)
 }
 
 // NewReleaseService initializes a new instance of the release Service using the provided dependencies.
-func NewReleaseService(releaseRepo release.ReleaseRepository, appRepo app.AppRepository) Service {
+func NewReleaseService(releaseRepo release.ReleaseRepository, releaseNotesRepo release.ReleaseNotesRepository, appRepo app.AppRepository) Service {
 	return &service{
-		releaseRepo: releaseRepo,
-		appRepo:     appRepo,
+		releaseRepo:      releaseRepo,
+		releaseNotesRepo: releaseNotesRepo,
+		appRepo:          appRepo,
 	}
 }
 
 type service struct {
-	releaseRepo release.ReleaseRepository
-	appRepo     app.AppRepository
+	releaseRepo      release.ReleaseRepository
+	releaseNotesRepo release.ReleaseNotesRepository
+	appRepo          app.AppRepository
+}
+
+// SaveReleaseNotes saves release notes and associated changelogs for a release
+func (s *service) SaveReleaseNotes(releaseID int, text string, changelogs []release.Changelog) (release.ReleaseNotes, error) {
+	releaseNotes := release.NewReleaseNotes(releaseID, text)
+	releaseNotes.Changelogs = changelogs
+
+	if err := s.releaseNotesRepo.Save(&releaseNotes); err != nil {
+		return release.ReleaseNotes{}, err
+	}
+
+	return releaseNotes, nil
+}
+
+// GetReleaseNotes retrieves release notes and associated changelogs for a release
+func (s *service) GetReleaseNotes(releaseID int) (release.ReleaseNotes, error) {
+	releaseNotes, err := s.releaseNotesRepo.FindByReleaseID(releaseID)
+	if err != nil {
+		return release.ReleaseNotes{}, err
+	}
+
+	return releaseNotes, nil
 }
